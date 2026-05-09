06c4e532ca112b1dcd874300d7e861814b56b20d
EricZhang1412 <1197609377@qq.com>
2026-05-10 03:32:18 +0800

    Modified

--- multigpu_train.sh
+++ multigpu_train.sh
@@ -2,7 +2,7 @@
 # Multi-GPU / multi-node training script (DDP via torchrun)
 # Usage: bash multigpu_train.sh [model_config] [data_config] [gpus_per_node] [num_nodes]
 # Example: bash multigpu_train.sh spiking_resnet18 imagenet 8 1
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=3,4,5,6
 
 PROJECT=${1:-default_project_configs}
 TRAINING=${2:-transformer}
@@ -10,7 +10,7 @@ MODEL=${3:-billeh_v1_seqcifar10}
 DATA=${4:-sequential_cifar10_fewshot}
 OPTIMIZER=${5:-transformer}
 
-GPUS=${6:-2}
+GPUS=${6:-4}
 NODES=${7:-1}
 PORT=${8:-29502}
 
